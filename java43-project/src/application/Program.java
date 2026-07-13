d0bdf2fefe701d00069d7fbbadf1c61bfbd0110a
Jose <j.lehnemann@gmail.com>
2026-07-12 21:57:16 -0300

    Updated java43-project - Seller entity class

--- java43-project/src/application/Program.java
+++ java43-project/src/application/Program.java
@@ -1,12 +1,26 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Program {
     public static void main(String[] args) {
 
+
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Department obj = new Department(1, "Books");
 
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com",
+                LocalDate.parse("21/11/1978", dtf1), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
