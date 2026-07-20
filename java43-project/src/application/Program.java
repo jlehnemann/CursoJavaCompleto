bceb3a5302963002f617a90dcfd30ae09eabfbdf
Jose <j.lehnemann@gmail.com>
2026-07-19 21:53:48 -0300

    Updated java43-project - implemented SellerDaoJDBC findById

--- java43-project/src/application/Program.java
+++ java43-project/src/application/Program.java
@@ -1,30 +1,23 @@
 package application;
 
-import model.entities.Department;
 import model.entities.Seller;
 import model.entities.dao.DaoFactory;
 import model.entities.dao.SellerDao;
 
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 
 
 public class Program {
     public static void main(String[] args) {
 
 
-        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com",
-                LocalDate.parse("21/11/1978", dtf1), 3000.0, obj);
 
         //instanciação usando DAOFactory
         SellerDao sellerDAO = DaoFactory.createSellerDAO();
+        Seller seller = sellerDAO.findById(3);
 
-        System.out.println(obj);
         System.out.println(seller);
     }
 }
--- java43-project/src/model/entities/dao/DaoFactory.java
+++ java43-project/src/model/entities/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.entities.dao;
 
+import db.DB;
 import model.entities.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     //faz com que não precise expor a implementação, somente a interface
     public static SellerDao createSellerDAO() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
 }
