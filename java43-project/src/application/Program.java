0db69a4fed7783e55759d23a836ac9eaaefb6fec
Jose <j.lehnemann@gmail.com>
2026-07-27 20:59:04 -0300

    Updated java43-project - update implementation

--- java43-project/src/application/Program.java
+++ java43-project/src/application/Program.java
@@ -40,10 +40,17 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 4: seller insert ===");
+        System.out.println("\n=== TEST 4: seller update ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.parse("21/11/1990", dtf), 4000.0, department);
         sellerDAO.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller insert ===");
+        seller = sellerDAO.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDAO.update(seller);
+        System.out.println("Update completed");
+
 
 
     }
