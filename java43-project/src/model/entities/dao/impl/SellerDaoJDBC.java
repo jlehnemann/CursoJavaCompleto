55f0c8126f165600b10c2c451cddca1d9a687e00
Jose <j.lehnemann@gmail.com>
2026-07-30 21:27:28 -0300

    Updated java43-project - refactored unnecessary imports from SellerDaoJDBC

--- java43-project/src/model/entities/dao/impl/SellerDaoJDBC.java
+++ java43-project/src/model/entities/dao/impl/SellerDaoJDBC.java
@@ -10,8 +10,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
