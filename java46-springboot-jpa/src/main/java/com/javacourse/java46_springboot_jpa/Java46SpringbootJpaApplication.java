ce2e2f3e4b91098b1f7510346845e49cf69c46a5
Jose <j.lehnemann@gmail.com>
2026-08-29 20:46:30 -0300

    Added java46-springboot-jpa - first steps

---
+++ java46-springboot-jpa/src/main/java/com/javacourse/java46_springboot_jpa/Java46SpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.javacourse.java46_springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java46SpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java46SpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ java46-springboot-jpa/src/test/java/com/javacourse/java46_springboot_jpa/Java46SpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacourse.java46_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java46SpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
