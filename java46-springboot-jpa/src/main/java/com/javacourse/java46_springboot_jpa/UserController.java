6e31377f82e32d997f27747840fea2da4487abc3
Jose <j.lehnemann@gmail.com>
2026-08-29 21:49:03 -0300

    Updated java46-springboot-jpa - User entity and resource

---
+++ java46-springboot-jpa/src/main/java/com/javacourse/java46_springboot_jpa/UserController.java
@@ -0,0 +1,19 @@
+package com.javacourse.java46_springboot_jpa;
+
+import com.javacourse.java46_springboot_jpa.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "99 99654-3210", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
